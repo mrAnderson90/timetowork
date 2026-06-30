d559ff37aa421d17b2f4092fe27ad9ec3b9fc2e8
operator-1 <tyler.andreev@mail.ru>
2026-06-30 18:43:24 +0300

    Fix middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\CheckApplicant;
+use App\Http\Middleware\CheckEmployer;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'applicant' => CheckApplicant::class,
+            'employer' => CheckEmployer::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
