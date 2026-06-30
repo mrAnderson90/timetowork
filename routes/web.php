c0716917575031d461d34fef11b199f739c63525
operator-1 <tyler.andreev@mail.ru>
2026-06-30 18:07:48 +0300

    Refactor navigation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ResumeController;
 use App\Http\Controllers\ResumeEducationController;
@@ -9,9 +11,16 @@ use App\Http\Controllers\ResumePhotoController;
 use App\Http\Controllers\VacancyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('vacancies.index');
-});
+/*
+|--------------------------------------------------------------------------
+| Главная
+|--------------------------------------------------------------------------
+*/
+
+Route::redirect('/', '/main');
+
+Route::get('/main', [MainController::class, 'index'])
+    ->name('main.index');
 
 /*
 |--------------------------------------------------------------------------
@@ -36,15 +45,8 @@ Route::middleware(['auth', 'applicant'])->group(function () {
         ->name('resume-experiences.')
         ->group(function () {
 
-            Route::get(
-                '/experiences/create',
-                [ResumeExperienceController::class, 'create']
-            )->name('create');
-
-            Route::post(
-                '/experiences',
-                [ResumeExperienceController::class, 'store']
-            )->name('store');
+            Route::get('/experiences/create', [ResumeExperienceController::class, 'create'])->name('create');
+            Route::post('/experiences', [ResumeExperienceController::class, 'store'])->name('store');
 
         });
 
@@ -52,20 +54,9 @@ Route::middleware(['auth', 'applicant'])->group(function () {
         ->name('resume-experiences.')
         ->group(function () {
 
-            Route::get(
-                '/{experience}/edit',
-                [ResumeExperienceController::class, 'edit']
-            )->name('edit');
-
-            Route::patch(
-                '/{experience}',
-                [ResumeExperienceController::class, 'update']
-            )->name('update');
-
-            Route::delete(
-                '/{experience}',
-                [ResumeExperienceController::class, 'destroy']
-            )->name('destroy');
+            Route::get('/{experience}/edit', [ResumeExperienceController::class, 'edit'])->name('edit');
+            Route::patch('/{experience}', [ResumeExperienceController::class, 'update'])->name('update');
+            Route::delete('/{experience}', [ResumeExperienceController::class, 'destroy'])->name('destroy');
 
         });
 
@@ -73,15 +64,8 @@ Route::middleware(['auth', 'applicant'])->group(function () {
         ->name('resume-educations.')
         ->group(function () {
 
-            Route::get(
-                '/educations/create',
-                [ResumeEducationController::class, 'create']
-            )->name('create');
-
-            Route::post(
-                '/educations',
-                [ResumeEducationController::class, 'store']
-            )->name('store');
+            Route::get('/educations/create', [ResumeEducationController::class, 'create'])->name('create');
+            Route::post('/educations', [ResumeEducationController::class, 'store'])->name('store');
 
         });
 
@@ -89,20 +73,9 @@ Route::middleware(['auth', 'applicant'])->group(function () {
         ->name('resume-educations.')
         ->group(function () {
 
-            Route::get(
-                '/{education}/edit',
-                [ResumeEducationController::class, 'edit']
-            )->name('edit');
-
-            Route::patch(
-                '/{education}',
-                [ResumeEducationController::class, 'update']
-            )->name('update');
-
-            Route::delete(
-                '/{education}',
-                [ResumeEducationController::class, 'destroy']
-            )->name('destroy');
+            Route::get('/{education}/edit', [ResumeEducationController::class, 'edit'])->name('edit');
+            Route::patch('/{education}', [ResumeEducationController::class, 'update'])->name('update');
+            Route::delete('/{education}', [ResumeEducationController::class, 'destroy'])->name('destroy');
 
         });
 
@@ -110,15 +83,8 @@ Route::middleware(['auth', 'applicant'])->group(function () {
         ->name('resume-photos.')
         ->group(function () {
 
-            Route::get(
-                '/photos/create',
-                [ResumePhotoController::class, 'create']
-            )->name('create');
-
-            Route::post(
-                '/photos',
-                [ResumePhotoController::class, 'store']
-            )->name('store');
+            Route::get('/photos/create', [ResumePhotoController::class, 'create'])->name('create');
+            Route::post('/photos', [ResumePhotoController::class, 'store'])->name('store');
 
         });
 
@@ -126,15 +92,8 @@ Route::middleware(['auth', 'applicant'])->group(function () {
         ->name('resume-photos.')
         ->group(function () {
 
-            Route::patch(
-                '/{photo}',
-                [ResumePhotoController::class, 'update']
-            )->name('update');
-
-            Route::delete(
-                '/{photo}',
-                [ResumePhotoController::class, 'destroy']
-            )->name('destroy');
+            Route::patch('/{photo}', [ResumePhotoController::class, 'update'])->name('update');
+            Route::delete('/{photo}', [ResumePhotoController::class, 'destroy'])->name('destroy');
 
         });
 
@@ -142,15 +101,8 @@ Route::middleware(['auth', 'applicant'])->group(function () {
         ->name('applications.')
         ->group(function () {
 
-            Route::get(
-                '/applications/create',
-                [ApplicationController::class, 'create']
-            )->name('create');
-
-            Route::post(
-                '/applications',
-                [ApplicationController::class, 'store']
-            )->name('store');
+            Route::get('/applications/create', [ApplicationController::class, 'create'])->name('create');
+            Route::post('/applications', [ApplicationController::class, 'store'])->name('store');
 
         });
 
@@ -158,20 +110,9 @@ Route::middleware(['auth', 'applicant'])->group(function () {
         ->name('applications.')
         ->group(function () {
 
-            Route::get(
-                '/{application}/edit',
-                [ApplicationController::class, 'edit']
-            )->name('edit');
-
-            Route::patch(
-                '/{application}',
-                [ApplicationController::class, 'update']
-            )->name('update');
-
-            Route::delete(
-                '/{application}',
-                [ApplicationController::class, 'destroy']
-            )->name('destroy');
+            Route::get('/{application}/edit', [ApplicationController::class, 'edit'])->name('edit');
+            Route::patch('/{application}', [ApplicationController::class, 'update'])->name('update');
+            Route::delete('/{application}', [ApplicationController::class, 'destroy'])->name('destroy');
 
         });
 
@@ -184,6 +125,8 @@ Route::middleware(['auth', 'applicant'])->group(function () {
 */
 
 Route::middleware(['auth', 'employer'])->group(function () {
+
+    Route::resource('companies', CompanyController::class);
 
     Route::resource('vacancies', VacancyController::class)
         ->except(['index', 'show']);
@@ -198,20 +141,9 @@ Route::middleware(['auth', 'employer'])->group(function () {
 
 Route::middleware('auth')->group(function () {
 
-    Route::get(
-        '/profile',
-        [ProfileController::class, 'edit']
-    )->name('profile.edit');
-
-    Route::patch(
-        '/profile',
-        [ProfileController::class, 'update']
-    )->name('profile.update');
-
-    Route::delete(
-        '/profile',
-        [ProfileController::class, 'destroy']
-    )->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
 });
 
